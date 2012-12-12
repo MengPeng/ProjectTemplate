5b65534cd7ca87a57d7293f5fbbb007c80b7aade
mengpeng <ampy.apple@gmail.com>
2012-12-12 14:41:41 +0800

    init

---
+++ ARC/iOSTemplate/iOSTemplate/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iOSTemplate
+//
+//  Created by  rtsafe02 on 12-12-12.
+//  Copyright (c) 2012年 TelSafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ARC/iOSTemplate/iOSTemplate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSTemplate
+//
+//  Created by  rtsafe02 on 12-12-12.
+//  Copyright (c) 2012年 TelSafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
