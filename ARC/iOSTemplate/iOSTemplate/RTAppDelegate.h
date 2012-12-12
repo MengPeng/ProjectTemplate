3858b44a25e8161c4863f4d76c65bb4c960f2fce
mengpeng <ampy.apple@gmail.com>
2012-12-12 16:34:34 +0800

    init

---
+++ ARC/iOSTemplate/iOSTemplate/RTAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RTAppDelegate.h
+//  iOSTemplate
+//
+//  Created by  rtsafe02 on 12-12-12.
+//  Copyright (c) 2012年 TelSafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RTViewController;
+
+@interface RTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) RTViewController *viewController;
+
+@end
---
+++ ARC/iOSTemplate/iOSTemplate/RTViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTViewController.h
+//  iOSTemplate
+//
+//  Created by  rtsafe02 on 12-12-12.
+//  Copyright (c) 2012年 TelSafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTViewController : UIViewController
+
+@end
